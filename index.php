b9f88299b48949e1722ec7f04465a5180b2bb5ea
edufeq03 <30552031+edufeq03@users.noreply.github.com>
2022-10-14 19:44:13 -0300

    link formulario navbar

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <div class="nav-wrapper container">
       <a id="logo-container" href="#" class="brand-logo">Logo</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="formulario.html">Preencher Formulário</a></li>
       </ul>
 
       <ul id="nav-mobile" class="sidenav">
